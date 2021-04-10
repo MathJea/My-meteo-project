878be8933d82fb6392a29118f82ad4201cf59b4b
MathJea <mathjeann@gmail.com>
2021-04-10 20:24:35 +0200

    Icon

--- src/index.js
+++ src/index.js
@@ -69,7 +69,15 @@ function showTemperature(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
-}
+  }
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+// HandleSubmit
 
 function handleSubmit(event) {
   event.preventDefault();
